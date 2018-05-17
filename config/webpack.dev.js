41de5cda1e22272109ff4dfd03bda937b4f77417
Marat Zh <regeon-93@inbox.ru>
2018-05-18 01:20:27 +0500

    include images-loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -36,9 +36,20 @@ module.exports = {
                     loader: "extract-loader"
                 },
                 {
-                    loader: "html-loader"
+                    loader: "html-loader",
+                    options: {
+                        attrs: ["img:src"]
+                    }
                 }
             ]
+        }, {
+            test: /\.(jpg|gif|png)$/,
+            use: [{
+                loader: "file-loader",
+                options: {
+                    name: "images/[name].[ext]"
+                }
+            }]
         }]
     }
 };
