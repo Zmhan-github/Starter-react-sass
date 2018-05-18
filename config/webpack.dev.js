b606f7f13084cbe675c82bff9648973f043595cd
Marat Zh <regeon-93@inbox.ru>
2018-05-18 13:42:51 +0500

    HOT reloading Both Client and Server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -38,21 +39,8 @@ module.exports = {
         }, {
             test: /\.html$/,
             use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].html"
-                    }
-                },
-                {
-                    loader: "extract-loader"
-                },
-                {
-                    loader: "html-loader",
-                    options: {
-                        attrs: ["img:src"]
-                    }
-                }
-            ]
+                loader: "html-loader",
+            }]
         }, {
             test: /\.(jpg|gif|png)$/,
             use: [{
@@ -64,6 +52,9 @@ module.exports = {
         }]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        })
     ]
 };
