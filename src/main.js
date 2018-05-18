d7d2766c9a912301c817dbccc63ea67d336d65b2
Marat Zh <regeon-93@inbox.ru>
2018-05-18 11:40:32 +0500

    include babel-polyfills, Transforms & Presets

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+require("babel-runtime/regenerator")
 require("./main.css")
 require("./index.html")
 
-var a = () => {
-    console.log("Hello from the future!");
+var a = async args => {
+    const {
+        a,
+        b
+    } = args;
+    await console.log("Hello from the future!", a, b);
+    console.log("Done!");
 }
 
-a();
+a({
+    a: 1,
+    b: 2
+});
