2d57591f68a34814280fed77a86740ff7f72d8cb
Marat Zh <regeon-93@inbox.ru>
2018-05-18 14:06:22 +0500

    include chrome-debugger

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-require("babel-runtime/regenerator")
+require("babel-runtime/regenerator");
 require("webpack-hot-middleware/client?reload=true");
-require("./main.css")
-require("./index.html")
+require("./main.css");
+require("./index.html");
