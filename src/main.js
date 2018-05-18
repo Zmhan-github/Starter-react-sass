dd2f2b0dbc05540d3bc3a078b496a33bad0915c7
Marat Zh <regeon-93@inbox.ru>
2018-05-18 14:48:49 +0500

    include react-hot-loader

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 require("babel-runtime/regenerator");
+require("react-hot-loader/patch");
+require("babel-register");
 require("webpack-hot-middleware/client?reload=true");
 require("./main.css");
+require("./images/otter1.jpg");
 require("./index.html");
+require("./app")
