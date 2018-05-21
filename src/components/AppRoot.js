1548c26ed3afb701265e96ccc2ff4eb13400d99f
Marat Zh <regeon-93@inbox.ru>
2018-05-21 21:15:07 +0500

    add man

--- src/components/AppRoot.js
+++ src/components/AppRoot.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import moon from '../data/moon.html';
+import lest from '../data/lest.html'
+import man from '../data/man.html'
 
 export default class extends Component {
  
     render() {
         return (
-        <header className="header-content">
+        <header className="header">
             <div className="text-header">Some text</div>    
             <div className="cloud fcloud01"></div>
-            <div className="cloud fcloud02"></div>
+            <div className="cloud fcloud02"></div> 
+            <div className="svg-content" dangerouslySetInnerHTML={{ __html: moon}} />  
+            <div className="svg-content" dangerouslySetInnerHTML={{ __html: lest}} />  
+            <div className="svg-content" dangerouslySetInnerHTML={{ __html: man}} />
         </header>
         )
     }
